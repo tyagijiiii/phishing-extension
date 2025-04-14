db2cc0d70de1b533b7ba85331d200908ba2cfca5
tyagijiiii <vatsaltyagi588@gmail.com>
2025-04-14 14:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def predict():
 
 # Run the Flask App
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
